e61910dcffa7aa34420289c4390c818424e5afbe
nievescolantonio <nievescolantonio@hotmail.com>
2022-08-17 16:10:34 -0300

    se agrega al proyecto

--- familiares/views.py
+++ familiares/views.py
@@ -2,13 +2,28 @@ from operator import and_
 from django.shortcuts import render
 from django.http import HttpResponse
 from familiares.models import Familiares
-
+from django.template import Template, Context
 # Create your views here.
+
+
 
 def lista_familiares(request):
     familiares = Familiares.objects.all()
     lista_familiares_nombre = []
     lista_familiares_fecha_nacimiento = []
+
+    mi_template = open (r"C:\django\DJANGO\MVT_ColantonioNieves\MVT_ColantonioNieves\template\index.html", "r")
+
+    plantilla = Template(mi_template.read())
+
+    mi_template.close()
+
+    context= Context()
+
+    documento_a_devolver = plantilla.render (context)
+
+    return HttpResponse(documento_a_devolver)
+
 
     for familiares in familiares:
         lista_familiares_nombre.append(familiares.nombre)
